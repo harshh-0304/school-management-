11736b19bc3b3d125e84a8864915736376b5044f
harshh <bakasakuta33@gmail.com>
2025-06-28 17:56:38 +0530

    3

--- schoolmanagement/obj/Debug/net9.0/schoolmanagement.AssemblyInfo.cs
+++ schoolmanagement/obj/Debug/net9.0/schoolmanagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("schoolmanagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfda6141e73ad5be1f30496b965a79915fdbbdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026c0238ef8e349279876148360f3ce4a0c80b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("schoolmanagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("schoolmanagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
